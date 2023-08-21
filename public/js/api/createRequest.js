fa60345941ef65bcf3469f0c43be547d6880d8b0
Zunickx <Zunickx@gmail.com>
2023-08-21 23:27:00 +0300

    Create createRequest ver 1.0

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,17 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
-
-};
+const createRequest = () => {
+    const xhr = new XMLHttpRequest;
+    xhr.open( 'GET', 'https://example.com?mail=ivan@poselok.ru&password=odinodin' ); // открываем новое соединение и отправляем данные пользователя для авторизации на сервере
+    xhr.send();                                                                      // отправляем запрос на сервер
+    xhr.responseType = 'json';                                                       // устанавливаем что ответ сервера будет в формате json
+  
+    callback = (err, response) => {
+      if(!xhr.response.err) {                                                        // если нет ошибки, то выводим в консоль данные с сервера
+        response = console.log(xhr.response.data);
+      } else {
+        err = console.log(xhr.response.err);                                         // если есть ошибка, то выводим в консоль ошибку
+      }
+    }
+  };
