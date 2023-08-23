772c7a92cc3a2a3bab58c5d3902a4da727049f70
Zunickx <Zunickx@gmail.com>
2023-08-23 22:52:48 +0300

    Create Sidebar ver 1.0

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,7 +18,12 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
-
+    const app = document.querySelector('.app')
+    const sidebarToggle = document.querySelector('.sidebar-toggle');
+    sidebarToggle.onclick = function() {
+        app.classList.toggle('sidebar-open');
+        app.classList.toggle('sidebar-collapse');
+    }
   }
 
   /**
@@ -29,6 +34,23 @@ class Sidebar {
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
+    const menuRegister = document.querySelector('.menu-item_register');
+    menuRegister.addEventListener('click', (event) => {
+        App.getModal('register').open();
+    })
 
+    const menuLogin = document.querySelector('.menu-item_login');
+    menuLogin.addEventListener('click', (event) => {
+        App.getModal('login').open();
+    })
+
+    const menuItemLogout = document.querySelector('.menu-item_logout');
+    menuItemLogout.addEventListener('click', (event) => {
+        User.logout((err, response) => {
+            if (response.success) {
+                App.setState('init');
+            }
+        });
+    })
   }
 }
